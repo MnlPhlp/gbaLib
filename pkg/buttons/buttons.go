73f535d839b2be3765733f8db72f111fdd1e5df2
MnlPhlp <online@manuel.sh>
2021-04-07 16:21:17 +0200

    added clicked function wich is true once per poll

--- pkg/buttons/buttons.go
+++ pkg/buttons/buttons.go
@@ -26,19 +26,41 @@ const (
 // initialize both states with no buttons pressed
 var last ButtonState = 0x3FF
 var current ButtonState = 0x3FF
+var clicked ButtonState = 0x3FF
 
 func (state ButtonState) isDown(button Button) bool {
 	// key bits are active low
 	return uint16(state)&uint16(button) == 0
 }
 
+func (state *ButtonState) setDown(b Button) {
+	*state = *state & ButtonState(^b)
+}
+
 func (button Button) IsPressed() bool {
 	return current.isDown(button)
+}
+
+func (b Button) WasPressed() bool {
+	return last.isDown(b)
+}
+
+func (b Button) Released() bool {
+	return b.WasPressed() && !b.IsPressed()
+}
+
+func (b Button) Clicked() bool {
+	if b.Released() && !clicked.isDown(b) {
+		clicked.setDown(b) //mark klicked, so clicked() is not true for next call
+		return true
+	}
+	return false
 }
 
 func Poll() {
 	last = current
 	current = ButtonState(registers.Key.KeyPad.Get())
+	clicked = 0x3FF // reset clicked store
 }
 
 func keyIsr(interrupt.Interrupt) {
@@ -52,4 +74,11 @@ func EnableAutoPolling() {
 	registers.Key.KeyCnt.SetBits(0b1111111111)
 	// create a new Interrupt and store the function
 	interrupt.New(machine.IRQ_KEYPAD, keyIsr).Enable()
+}
+
+func (b Button) Wait() {
+	Poll()
+	for !b.Clicked() {
+		Poll()
+	}
 }
